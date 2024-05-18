6674040890963ec20603dd6ecbdd6cf1a9c9047e
hansj101201 <hansjerremy57@gmail.com>
2024-05-18 11:26:14 +0700

    email queue

--- app/Http/Controllers/stkjadiController.php
+++ app/Http/Controllers/stkjadiController.php
@@ -54,6 +54,7 @@ class stkjadiController extends Controller
             ->where('stkjadi.ID_GUDANG', $idGudang)
             ->join('barang', 'stkjadi.ID_BARANG', '=', 'barang.ID_BARANG')
             ->where('barang.ACTIVE', 1)
+            ->where('stkjadi.SALDO','!=',0)
             ->join('satuan', 'barang.ID_SATUAN', 'satuan.ID_SATUAN')
             ->join('harga', function ($join) use ($tanggalFormatted) {
                 $join->on('barang.ID_BARANG', '=', 'harga.ID_BARANG')
